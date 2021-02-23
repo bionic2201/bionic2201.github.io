b6c577e37248dffe3ddd078687e5c04a6c54c55b
bionic2201 <63063949+bionic2201@users.noreply.github.com>
2021-02-23 17:37:00 +0100

    counter

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -37,7 +37,26 @@ jQuery(document).ready(function() {
     */
     new WOW().init();
 	
-//Get the button
+//counter
+
+$(document).ready(function(){
+    $('.counter-value').each(function(){
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        },{
+            duration: 3500,
+            easing: 'swing',
+            step: function (now){
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
+
+
+
+
+	//Get the button
 var btn = $('#button');
 
 $(window).scroll(function() {
