f14a6d3728832d6b1171a672ed42b6a6284654d4
bionic2201 <63063949+bionic2201@users.noreply.github.com>
2021-02-23 14:37:16 +0100

    last

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -37,6 +37,22 @@ jQuery(document).ready(function() {
     */
     new WOW().init();
 	
+//Get the button
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
 
 
 $(".nav-tabs li.nav-item a.nav-link").click(function() {
